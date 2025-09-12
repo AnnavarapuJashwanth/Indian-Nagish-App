99fd2e3d73db1f9f14978f1cf7f51f302d5c78be
Annavarapu Jashwanth <jashwanthannavarapu99@gmail.com>
2025-09-13 00:05:46 +0530

    last update

--- nagish/src/services/config.js
+++ nagish/src/services/config.js
@@ -1,2 +1,5 @@
 // src/config.js
-export const API_BASE_URL = "https://indian-nagish-app-1.onrender.com/api";
+console.log("API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
+
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
--- nagish/src/services/translateService.js
+++ nagish/src/services/translateService.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { API_BASE_URL } from "./config";
 
-const API_URL = `${API_BASE_URL}/translate`;
-
 export const translateText = async (text, target = "hi") => {
   try {
-    const res = await axios.post(API_URL, { text, target });
+    const res = await axios.post(`${API_BASE_URL}/translate`, { text, target });
     return res.data.translatedText;
   } catch (err) {
     console.error("Translate API error:", err.response?.data || err.message);
